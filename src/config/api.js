f7e45c518f99f4783edfebdf30b19fbfd774c402
marinagosson <marinagosson@snowmanlabs.com>
2026-07-08 20:25:17 -0300

    chore: change url

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export const API_URL =
     window.location.hostname === "localhost"
         ? "http://localhost:3000"
-        : "https://SEU-SERVICO.onrender.com";
+        : "https://netflix-recommendation-api-r15g.onrender.com";
 
 export const BATCH_SIZE = 100;
